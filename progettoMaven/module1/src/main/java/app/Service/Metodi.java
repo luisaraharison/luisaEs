7916341eae33b1d2d25c160bd382fbad472b9048
Your Name <you@example.com>
2024-03-18 12:51:40 +0100

    qualcosa

--- progettoMaven/module1/src/main/java/app/Service/Metodi.java
+++ progettoMaven/module1/src/main/java/app/Service/Metodi.java
@@ -8,10 +8,13 @@ import app.Entity.Attributi;
 public class Metodi {
     private List<Attributi> attributi;
     
-
     public Metodi(){
         attributi = new ArrayList<>();//creo una lista
-        attributi.add(null);
+        Attributi n = new Attributi();
+        n.setAttributo1("Attributo 1 ");
+        n.setAttributo2("Attributo 2 ");
+        n.setAttributo3("Attributo 3 ");
+        attributi.add(n);
     }
 
     public void metodo1() {
@@ -23,9 +26,18 @@ public class Metodi {
     for (Attributi t : attributi) {
         if(t.getAttributo1().equals(a)) {
             t.setAttributo1(b);
-            System.out.println(t.getAttributo1());
+            System.out.println(t.getAttributo1());//modifica l'attributo 1 in qualcos'altro
             }
         }
     }
+
+    public void metodo3(String a,String b) {
+        for (Attributi t : attributi) {
+            if(t.getAttributo2().equals(a)) {
+                t.setAttributo2(b);
+                System.out.println(t.getAttributo2());//modifica l'attributo 2 in qualcos'altro
+                }
+            }
+        }
 }
 
--- progettoMaven/module1/src/main/java/app/Utility/MetodiP.java
+++ progettoMaven/module1/src/main/java/app/Utility/MetodiP.java
@@ -1,13 +1,14 @@
 package app.Utility;
 
-import app.Entity.Attributi;
 import app.Service.Metodi;
 
 public class MetodiP {
     public void altriMetodi() {
         Metodi m = new Metodi();
         m.metodo1();
+        m.metodo2("Attributo 1 ", "Attributo uno");
+        m.metodo3("Attributo 2 ","Attributo due");
     }
-   Attributi t = new Attributi();
-   //t.setAttributo1("Attributo 1");
+   
+   
 }
